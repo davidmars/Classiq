d06dac5517aa322b4dba2ea56a793dd2153936ac
david <d.marsalone@gmail.com>
2018-02-16 18:28:27 +0100

    css reset

--- Classiq/_src/unique-instances/layout-stuff.php
+++ Classiq/_src/unique-instances/layout-stuff.php
@@ -1,4 +1,8 @@
-<div id="the-cq-layer" class="cq-vars-th-white">
+<script>
+    var CqLayer = document.registerElement('cq-layer');
+    //document.body.appendChild(new CqLayer());
+</script>
+<cq-layer id="the-cq-layer" class="cq-vars-th-white cq-css">
     <?=$view->render("./cq-big-menu/cq-big-menu")?>
     <?=$view->render("./cq-context-menu/cq-context-menu")?>
     <?=$view->render("./cq-notifier/wysiwyg-notifier")?>
@@ -6,4 +10,4 @@
         <?=pov()->svg->import("dist/svg-collection/cq.svg")?>
     </div>
     <?=$view->render("./cq-edit-record-box/cq-edit-record-box")?>
-</div>
+</cq-layer>
