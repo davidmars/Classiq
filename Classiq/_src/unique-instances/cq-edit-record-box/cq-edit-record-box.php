5113a0757b199016d7cd117b9bce78f4f8e681dc
davidmars <d.marsalone@gmail.com>
2018-05-27 14:30:09 +0200

    .cq-cols-big .cq-cols > .cq-col-6-bigonly pour avoir un colonnage différent selon où c'est utilisé.

--- Classiq/_src/unique-instances/cq-edit-record-box/cq-edit-record-box.php
+++ Classiq/_src/unique-instances/cq-edit-record-box/cq-edit-record-box.php
@@ -6,7 +6,7 @@
  * Time: 16:01
  */
 ?>
-<div id="the-cq-record-editor" cq-edit-record-box>
+<div id="the-cq-record-editor" class="cq-cols-big" cq-edit-record-box>
     <div cq-toolbar>
         <div>...</div>
         <button class="unstyled" cq-on-click="ui.recordEditor.hide()"><?=pov()->svg->use("cq-close")?></button>
