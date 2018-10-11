825a0b64cbfca12008b94e4100d829b0f52456b1
davidmars <d.marsalone@gmail.com>
2018-10-11 06:07:38 +0200

    othographe

--- Classiq/_src/cq-debug/WebpackVersion.js
+++ Classiq/_src/cq-debug/WebpackVersion.js
@@ -1,3 +1,3 @@
-//logue simplement la version webpack et si c'est une version de prod ou non
+//logue simplement la version webpack et si c'est une version de prod ou non.
 console.log("webpack version...",webpackVersion);
 console.log("webpack PRODUCTION...",PRODUCTION);
