f9562d6834cb713a70e756db39b83f97228b8044
david <d.marsalone@gmail.com>
2018-10-24 13:49:27 +0200

    default.wysiwyg.preview manquait l'icone d'erreur

--- Classiq/_src/records/default.wysiwyg.preview.php
+++ Classiq/_src/records/default.wysiwyg.preview.php
@@ -10,6 +10,7 @@ use Classiq\Models\Classiqmodel;
 <div class="preview-record" <?=$view->attrRefresh($vv->uid())?>>
     <?if($vv->id):?>
         <span class="icon"><?= pov()->svg->use(ClassicModelSchema::icon($vv))?></span>
+        <?=$view->render("./tip-errors")?>
         <div>
             <div class="title" title="<?=$vv->name?>"><?=$vv->name?></div>
             <div class="type"><?=$vv->modelType()?>@<?=$vv->id?></div>
