0151cf9c73c907b0b2f6e63522482e767851ce3d
davidmars <d.marsalone@gmail.com>
2019-02-06 07:10:37 +0100

    FieldString->htmlTag argument $displayIfEmpty Permet de ne pas afficher un balise html si le champs est vide

--- Classiq/Wysiwyg/FieldsTyped/FieldString.php
+++ Classiq/Wysiwyg/FieldsTyped/FieldString.php
@@ -17,6 +17,91 @@ class FieldString extends FieldTyped
     }
 
 
+
+    /**
+     * Dit que le champ est traduit dans la langue donnée
+     * @param string $langCode L'identifiant de langue
+     */
+    public function isTranslated($langCode){
+        $this->attr()["data-lang"]=$langCode;
+        return $this;
+    }
+
+    /**
+     * Définit un placeholder
+     * @param $placeholder
+     * @return $this
+     */
+    public function setPlaceholder($placeholder)
+    {
+        if($this->field->wysiwyg->active){
+            $this->attr()["placeholder"]=$placeholder;
+        }
+        return $this;
+
+    }/**
+     * Définit les boutons pour Medium editor
+     * @param string[] $buttons exemple ["h1","h2","bold","italic","anchor","select-record","removeFormat"]
+     * @return $this
+     */
+    public function setMediumButtons($buttons)
+    {
+        if($this->field->wysiwyg->active){
+            $this->options["mediumButtons"]=pov()->utils->array->fromString($buttons);
+        }
+        return $this;
+
+    }
+
+    /**
+     * Permet de définir quel type de records il est possible d'ajouter en href (n'a d'incidence que sur les champs RichText)
+     * @param string $recordsTypes Les types de records séparé par une virgune page,custommodel,etc
+     * @return $this
+     */
+    public function setSelectableRecordTypes($recordsTypes){
+        if($this->field->wysiwyg->active){
+            $this->options["selectableRecordsTypes"]=strtolower(implode(",",pov()->utils->array->fromString($recordsTypes)));
+        }
+        return $this;
+    }
+
+    /**
+     * Dit que le champ texte peut être rechargé même si le focus de la souris est toutjours dessus
+     */
+    public function dontCareFocus(){
+        $this->attr()["refresh-dont-care-focus"]="true";
+        return $this;
+    }
+    private $defaultValue="";
+
+    /**
+     * Permet de définir une valeur par défaut.
+     * ça ne va pas enregistrer la valeur (sauf si le champ est enregistré par la suite), mais ça l'affichera dans le champ texte.
+     * @param string $value
+     * @return $this
+     */
+    public function setDefaultValue($value)
+    {
+        $this->defaultValue=$value;
+        return $this;
+    }
+
+
+    //--------------------------final methods------------------------------------------------
+
+    /**
+     * Permet d'obtenir un tag Html
+     * @param string $tag
+     * @param bool $displayIfEmpty Si false et qu'on est pas en wysiwyg et que le texte est vide ne renverra pas de tag
+     * @return HtmlTag|string
+     */
+    public function htmlTag($tag="span",$displayIfEmpty=true){
+        $tag=parent::htmlTag($tag);
+        $tag->setInnerHTML($this->field->value(true,$this->defaultValue));
+        $tag->isRenderable=$displayIfEmpty || $tag->getInnerHTML() || $this->field->wysiwyg->active;
+        return $tag;
+    }
+
     /**
      * Permet d'obtenir un tag html INPUT
      * @param string $type L'attribut type qui va avec la balise input (text, number, date etc...)
@@ -79,7 +164,7 @@ class FieldString extends FieldTyped
     }
 
     /**
-      Permet d'obtenir un tag html TEXTAREA
+    Permet d'obtenir un tag html TEXTAREA
      * @param string $placeholder
      * @param string $class
      * @return HtmlTag|string
@@ -91,7 +176,7 @@ class FieldString extends FieldTyped
         return $tag;
     }
     /**
-      Permet d'obtenir un tag html div.fld.textarea Autrement dit un champ texte enrichi qui ressemble à un textarea
+    Permet d'obtenir un tag html div.fld.textarea Autrement dit un champ texte enrichi qui ressemble à un textarea
      * @param string $placeholder
      * @param string $class
      * @return HtmlTag|string
@@ -101,84 +186,6 @@ class FieldString extends FieldTyped
         $tag=$this->htmlTag("div");
         $tag->addClass($class);
         return $tag;
-    }
-    /**
-     * Dit que le champ est traduit dans la langue donnée
-     * @param string $langCode L'identifiant de langue
-     */
-    public function isTranslated($langCode){
-        $this->attr()["data-lang"]=$langCode;
-        return $this;
-    }
-
-    /**
-     * Permet d'obtenir un tag Html
-     * @param string $tag
-     * @return HtmlTag|string
-     */
-    public function htmlTag($tag="span"){
-        $tag=parent::htmlTag($tag);
-        $tag->setInnerHTML($this->field->value(true,$this->defaultValue));
-        return $tag;
-    }
-
-    /**
-     * Définit un placeholder
-     * @param $placeholder
-     * @return $this
-     */
-    public function setPlaceholder($placeholder)
-    {
-        if($this->field->wysiwyg->active){
-            $this->attr()["placeholder"]=$placeholder;
-        }
-        return $this;
-
-    }/**
-     * Définit les boutons pour Medium editor
-     * @param string[] $buttons exemple ["h1","h2","bold","italic","anchor","select-record","removeFormat"]
-     * @return $this
-     */
-    public function setMediumButtons($buttons)
-    {
-        if($this->field->wysiwyg->active){
-            $this->options["mediumButtons"]=pov()->utils->array->fromString($buttons);
-        }
-        return $this;
-
-    }
-
-    /**
-     * Permet de définir quel type de records il est possible d'ajouter en href (n'a d'incidence que sur les champs RichText)
-     * @param string $recordsTypes Les types de records séparé par une virgune page,custommodel,etc
-     * @return $this
-     */
-    public function setSelectableRecordTypes($recordsTypes){
-        if($this->field->wysiwyg->active){
-            $this->options["selectableRecordsTypes"]=strtolower(implode(",",pov()->utils->array->fromString($recordsTypes)));
-        }
-        return $this;
-    }
-
-    /**
-     * Dit que le champ texte peut être rechargé même si le focus de la souris est toutjours dessus
-     */
-    public function dontCareFocus(){
-        $this->attr()["refresh-dont-care-focus"]="true";
-        return $this;
-    }
-    private $defaultValue="";
-
-    /**
-     * Permet de définir une valeur par défaut.
-     * ça ne va pas enregistrer la valeur (sauf si le champ est enregistré par la suite), mais ça l'affichera dans le champ texte.
-     * @param string $value
-     * @return $this
-     */
-    public function setDefaultValue($value)
-    {
-        $this->defaultValue=$value;
-        return $this;
     }
 
 }
